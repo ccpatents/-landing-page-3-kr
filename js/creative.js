a998c9aabff0a5402e99f98b46496e63e4904c07
joondong2 <kwonjoondong@gmail.com>
2019-12-19 15:35:46 +0900

    .

--- js/creative.js
+++ js/creative.js
@@ -17,11 +17,11 @@ let ps_selected = false; // '특허 검색 왜..?'
     if (filter.indexOf(navigator.platform.toLowerCase()) < 0) {
       //mobile
       document.getElementById("share-button").style.display = "block";
-      document.getElementById("store-button2").style.display = "none";
+      document.getElementById("store-area2").style.display = "none";
       document.getElementById("win10_warn").style.display = "block";
     } else {
       //pc 
-      document.getElementById("store-button1").style.display = "block";
+      document.getElementById("store-area1").style.display = "block";
       document.getElementById("win_10_ads").style.display = "block";
     }
   }
