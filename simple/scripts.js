3d26740325427d4d74e6ae12b513033bbe76f985
joondong2 <kwonjoondong@gmail.com>
2020-03-04 02:22:45 +0900

    .

--- simple/scripts.js
+++ simple/scripts.js
@@ -1,5 +1,25 @@
+var sound = false;
+
+var ccpatents_player;
+var install_player;
+
 (function ($) {
-    $(".scroll").click(function(event){ event.preventDefault(); $('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000); });
+    $(".scroll").click(function (event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
+
+    $("#sound").click(function () {
+        soundTurn(sound);
+    });
+
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);    
 
     var selector = ".section";
 
@@ -72,3 +92,60 @@
         }
     );
 })(jQuery); // End of use strict
+
+function soundTurn(power) {
+    if (power) {
+        sound = false;
+    } else {
+        sound = true;
+    }
+
+    if (sound) {
+        $("#speaker").attr("src", "sound.png");
+        ccpatents_player.unMute();
+    } else {
+        $("#speaker").attr("src", "mute.png");
+        ccpatents_player.mute();
+    }
+}
+
+function onYouTubeIframeAPIReady() {
+    ccpatents_player = new YT.Player('ccpatents-video', {
+        width: '100%',
+        videoId: 'lH-aHi7pb0I',
+        events: {
+            'onReady': onCCPatentsReady,
+        }
+    });
+
+    ccpatents_player = new YT.Player('install-video', {
+        width: '100%',
+        videoId: '6GLS2SxLgqI',
+        events: {
+            'onReady': onInstallReady,
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onCCPatentsReady(event) {
+    ccpatents_player = event.target;
+    ccpatents_player.playVideo();
+    ccpatents_player.seekTo(27);
+    ccpatents_player.mute();
+    
+}
+
+function onInstallReady(event) {
+    install_player = event.target;
+}
+
+function click_download() {
+    ccpatents_player.stopVideo();
+    install_player.stopVideo();
+    install_player.playVideo();
+
+    setTimeout(function() {
+        window.location.href = "ms-windows-store://pdp/?productid=9N798V33QDF8";
+    },2000)
+}
